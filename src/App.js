ce60ad08e5784a6073ea027169fb4608e903ad86
farendivaz <dandyfariz9@gmail.com>
2022-03-12 05:39:27 +0700

    remote to github

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import logo from "./react.png";
 
 function App() {
   return (
     <>
       <h1>React Facts</h1>
       <p>some paragraph here</p>
+      <img src={logo} />
     </>
   );
 }
